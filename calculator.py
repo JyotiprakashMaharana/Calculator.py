58c28f19f978ebf3c2708b9fb93522b364ca5349
JyotiprakashMaharana <jyotiprakashmaharana123@gmail.com>
2026-03-24 16:44:55 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def calc(a,b):
+      print("sum=", a+b)
+      print("sub=", a-b)
+      print("mult=",a*b)
+      if b == 0:
+              print("Can not divide zero ") 
+      else:
+          print("div=", a/b)
+           
+calc(1,4)      
+      
+
